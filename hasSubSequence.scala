6e87abf6d2edd31edb36b1542e4cb16744a69fc6
Yewken <cfr2ak@gmail.com>
2017-11-08 22:06:19 +0900

    pattern match

--- hasSubSequence.scala
+++ hasSubSequence.scala
@@ -7,15 +7,19 @@
 def hasSubSequence[A](sup: List[A], sub: List[A]): Boolean = {
   // this is kind of double loop
   // go2 is count for as1 and go1 is count for as2
-  def go1(as1: List[A], as2: List[A]): Boolean = {
-    if (as2 == Nil) true
-    else if (as1.head == as2.head) go1(as1.tail, as2.tail)
-    else false// exit
+  def go1(as1: List[A], as2: List[A]): Boolean = as2 match {
+    case Nil => true
+    case _ => if (as1.head == as2.head) go1(as1.tail, as2.tail)
+              else false// exit
   }
-  def go2(as1: List[A], as2: List[A]): Boolean = {
-    if (as1 == Nil) false
-    else if(go1(as1, as2)) true
-    else go2(as1.tail, as2)
+  def go2(as1: List[A], as2: List[A]): Boolean = as1 match {
+    case Nil => false
+    case _ => if (go1(as1, as2)) true
+              else go2(as1.tail, as2)
   }
   go2(sup, sub)
+}
+
+def main(args: Array[String]): Unit = {
+  hasSubSequence(List(1, 2, 3, 4), List(4))
 }
