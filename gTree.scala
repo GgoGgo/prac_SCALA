79d1bdadd4d8562b0e24d261e486f3fabcfe9dba
Yewken <cfr2ak@gmail.com>
2017-11-09 23:41:18 +0900

    fold implementation

--- gTree.scala
+++ gTree.scala
@@ -13,22 +13,49 @@ object gTree {
     case Leaf(_) => 1
     case Branch(l,r) => 1 + size(l) + size(r)
   }
-  def maximum(t: gTree[Int]): Int = {
-    def go(t: gTree[Int], m: Int): Int = t match {
-      case Leaf(v) => v max m
-      case Branch(l,r) => go(l, m) max go(r, m)
-    }
-    go(t, 0)
+//  def maximum(t: gTree[Int]): Int = {
+//    def go(t: gTree[Int], m: Int): Int = t match {
+//      case Leaf(v) => v max m
+//      case Branch(l,r) => go(l, m) max go(r, m)
+//    }
+//    go(t, 0)
+//  }
+  def maximum(t: gTree[Int]): Int = t match {
+    case Leaf(v) => v
+    case Branch(l,r) => maximum(l) max maximum(r)
   }
-  def depth[A](t: gTree[A]): Int = {
-    def go(t: gTree[A], depth: Int, mDepth: Int): Int = t match {
-      case Leaf(_) => depth max mDepth
-      case Branch(l, r) => go(l, depth+1, mDepth) max go(r, depth+1, mDepth)
-    }
-    go(t,0,0)
+  // naive implementation
+//  def depth[A](t: gTree[A]): Int = {
+//    def go(t: gTree[A], depth: Int, mDepth: Int): Int = t match {
+//      case Leaf(_) => depth max mDepth
+//      case Branch(l, r) => go(l, depth+1, mDepth) max go(r, depth+1, mDepth)
+//    }
+//    go(t,0,0)
+//  }
+  // functional-native implementation
+  def depth[A](t: gTree[A]): Int = t match {
+    case Leaf(_) => 0
+    case Branch(l, r) => 1 + depth(l) max depth(r)
   }
-  def map[A,B](t: gTree[A], f: A => B): gTree[B] = t match {
-    case Leaf(v) => Leaf(f(v))
+  def map[A,B](t: gTree[A], f: A => gTree[B]): gTree[B] = t match {
+    case Leaf(v) => f(v)
     case Branch(l,r) => Branch(map(l,f), map(r,f))
+  }
+
+  def fold[A,B](t: gTree[A])(f: A => B)(g: (B,B) => B): B = t match {
+    case Leaf(v) => f(v)
+    case Branch(l,r) => g(fold(l)(f)(g), fold(r)(f)(g))
+  }
+  def sizeFold[A](t: gTree[A]): Int = {
+    fold(t)(_=>1)(_+_)
+  }
+  def maximumFold(t: gTree[Int]): Int = {
+    fold(t)(a=>a)(_ max _)
+  }
+  def depthFold[A](t: gTree[A]):Int = {
+    fold(t)(_=>0)((a,b) => 1 + a max b)
+  }
+  def mapFold[A,B](t: gTree[A], f: A => gTree[B]): gTree[B] = {
+    fold(t)(f)((a,b) => Branch(a,b))
   }
 }
