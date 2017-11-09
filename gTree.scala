b5e48d710dfe550fb1f777a06ef85e96bbeea751
Yewken <cfr2ak@gmail.com>
2017-11-09 22:38:27 +0900

    tree size

--- gTree.scala
+++ gTree.scala
@@ -8,3 +8,9 @@ sealed trait gTree[+A]
 case class Leaf[A](value: A) extends gTree[A]
 case class Branch[A](l: gTree[A], r: gTree[A]) extends gTree[A]
 
+object gTree {
+  def size[A](t: gTree[A]): Int = t match {
+    case Leaf(_) => 1
+    case Branch(l,r) => 1 + size(l) + size(r)
+  }
+}
