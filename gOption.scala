c2130a66850fd3e0e8584581d0c9e992c5d39bd0
Yewken <cfr2ak@gmail.com>
2017-11-10 21:57:03 +0900

    gOption

--- gOption.scala
+++ gOption.scala
@@ -4,11 +4,42 @@
 // Copyright 2017 GgoGgo, All rights reserved
 //
 
-sealed trait gOption[+A]
+//import scala.{Option => _}
+
+// it can't be loaded on scala repl
+// because interpreter ends its evaluation with braces
+// it brings the result which reports missing case class and case object
+sealed trait gOption[+A] {
+  def map[B](f: A => B): gOption[B] = this match {
+    case None => None
+    case Some(a) => Some(f(a))
+  }
+  def flatMap[B](f: A => gOption[B]): gOption[B] = this match {
+    case None => None
+    case Some(a) => f(a)
+  }
+  def getOrElse[B >: A](default: => B): B = this match {
+    case None => default
+    case Some(a) => a
+  }
+  def orElse[B >: A](ob: => gOption[B]): gOption[B] = this match {
+    case None => ob
+    case Some(a) => Some(a)
+  }
+  def filter(f: A => Boolean): gOption[A] = this match {
+    case None => None
+    case Some(a) => if (f(a)) Some(a)
+                    else None
+  }
+}
 case class Some[+A](get: A) extends gOption[A]
 case object None extends gOption[Nothing]
 
-def mean(xs: Seq[Double]): gOption[Double] = {
-  if (xs.isEmpty) None
-  else Some(xs.sum / xs.length)
+
+object gOption {
+  def mean(xs: Seq[Double]): gOption[Double] = {
+    if (xs.isEmpty) None
+    else Some(xs.sum / xs.length)
+  }
 }
+
