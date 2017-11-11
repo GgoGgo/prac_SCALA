df9db467a1307e98a6605611a2e561577f07f7bb
Yewken <cfr2ak@gmail.com>
2017-11-11 17:39:07 +0900

    package, Try, sequence

--- gOption.scala
+++ gOption.scala
@@ -4,7 +4,10 @@
 // Copyright 2017 GgoGgo, All rights reserved
 //
 
+// error occurs without package definition
+package prac.scala.gOption
 //import scala.{Option => _}
+import scala.{Option => _, Either => _, _}
 
 // it can't be loaded on scala repl
 // because interpreter ends its evaluation with braces
@@ -39,12 +42,30 @@ object gOption {
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
   }
-  def variance(xs: Seq[Double]): Option[Double] = {
+  def variance(xs: Seq[Double]): gOption[Double] = {
     mean(xs) flatMap(m => mean(xs.map(x => math.pow(x-m, 2))))
   }
   // any function (e.g. math functions) can be transform to operate on gOption context
   def lift[A,B](f: A => B): gOption[A] => gOption[B] = _ map f
-}
 
+  // naive implementation
+//  def map2[A,B,C](a: gOption[A], b: gOption[B])(f: (A,B) => C): gOption[C] = (a,b) match {
+//    case (None, _) => None
+//    case (_, None) => None
+//    case (Some(a_), Some(b_)) => Some(f(a_, b_))
+//  }
+  // functional implementation
+  def map2[A,B,C](a: gOption[A], b: gOption[B])(f: (A,B) => C): gOption[C] = a flatMap(aa => b map (bb => f(aa,bb)))
+
+  def sequence[A](as: List[gOption[A]]): gOption[List[A]] = as match {
+    case Nil => Some(Nil)
+    case x::xs => x flatMap (xx => sequence(xs) map (xx :: _))
+  }
+
+  def Try[A](a: => A): gOption[A] = {
+    try Some(a)
+    catch {case e: Exception => None}
+  }
+}
 
 
