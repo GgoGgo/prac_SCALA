108c2aab93d89cef7db79efd460ecd978b02f5aa
Yewken <cfr2ak@gmail.com>
2017-11-11 20:38:56 +0900

    Try, mean

--- gEither.scala
+++ gEither.scala
@@ -9,3 +9,16 @@ package prac.scala.temp
 sealed trait Either[+E,+A]
 case class Left[+E](value: E) extends Either[E, Nothing]
 case class Right[+A](value: A) extends Either[Nothing, A]
+
+object Either {
+  def mean(as: List[Double]): Either[String, Double] = as match {
+    case Nil => Left("it's Nil")
+    case _ => Right(as.sum/as.length)
+  }
+  // when want to get more specified information about error
+  // like stacktrace
+  def Try[A](a: A): Either[Exception, A] = {
+    try Right(a)
+    catch { case e: Exception => Left(e)}
+  }
+}
